b28a8e5d39fe49a41f2339f3dcc4d847387e8c66
HamzaNechi <91537201+HamzaNechi@users.noreply.github.com>
2023-10-05 00:38:42 +0100

    commit 5

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Nechi hamza < send mail test >");
+		System.out.println("Hello Nechi hamza send mail test");
 	}
 
 }
